b2a6be56dfa147ff6ed9e21a36133a0848497752
mattchristenson <matt@rcc.team>
2024-12-13 10:10:25 -0600

    initial commit

---
+++ src/main/java/com/example/gitexamples_githubfirst/GitexamplesGithubfirstApplication.java
@@ -0,0 +1,13 @@
+package com.example.gitexamples_githubfirst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitexamplesGithubfirstApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitexamplesGithubfirstApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gitexamples_githubfirst/GitexamplesGithubfirstApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gitexamples_githubfirst;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitexamplesGithubfirstApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
